59ecb03936fe6a4aa1f7e3ca6ae28c7f0a6b02a2
krishanu halder <84024641+krishanugithub2001@users.noreply.github.com>
2022-07-28 19:02:52 +0530

    Add files via upload

---
+++ Quiz Application with Timer/Quiz Application with Timer/js/questions.js
@@ -0,0 +1,72 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "In which year of First world war Germany declared war on Russia and France",
+    answer: "1915",
+    options: [
+      "1915",
+      "1914",
+      "1991",
+      "1988"
+    ]
+  },
+    {
+    numb: 2,
+    question: "How many Lok Sabha seats belong to Rajasthan?",
+    answer: "25",
+    options: [
+      "25",
+      "85",
+      "55",
+      "75"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which crop is sown on the largest area in India?",
+    answer: "Rice",
+    options: [
+      "Rice",
+      "Wheat",
+      "Sugarcane",
+      "coco"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Corey Anderson who has hit the fastest ODI century in 36 balls is from",
+    answer: "New Zealand",
+    options: [
+      "New Zealand",
+      "India",
+      "Englnd",
+      "West Indies"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
